be74d80c0a05ad1fe5b5281f1fd9ed61c10ec64d
Davi Vasconcelos Souza <davivasconcelossouza7@gmail.com>
2023-04-23 18:36:03 +0000

    feat: adding components folder to module resolver

--- App.tsx
+++ App.tsx
@@ -4,11 +4,12 @@ import { Routes } from "./src/routes";
 import {
   useFonts,
   Epilogue_400Regular,
+  Epilogue_600SemiBold,
   Epilogue_700Bold,
 } from "@expo-google-fonts/epilogue";
 import {LinearGradient} from 'expo-linear-gradient'
 
-import { Loading } from "./src/components/Loading";
+import { Loading } from "@components/Loading";
 import {THEME} from './src/theme'
 
 
@@ -16,6 +17,7 @@ export default function App() {
   const [fontsLoaded] = useFonts({
     Epilogue_400Regular,
     Epilogue_700Bold,
+    Epilogue_600SemiBold,
   });
 
   const config = {
