8e84834a34343f4e9d766ab8430d101aab0ae45b
Davi Vasconcelos Souza <davivasconcelossouza7@gmail.com>
2023-04-24 11:48:49 +0000

    feat: adding footer text

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,27 +1,31 @@
-import { Text, VStack } from "native-base";
+import { Pressable, Text, VStack } from "native-base";
 import { FormHeader } from "@components/FormHeader";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import { TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { ThirdPartyAuth } from "@components/ThirdPartyAuth";
 
 export function SignUp() {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   return (
     <VStack flex={1} bg="white">
       <FormHeader heading="Vamos Começar" text="Preencha os campos abaixo." />
-      <VStack mt={16}>
+      <VStack mt={14}>
         <Input placeholder="Nome completo" autoCapitalize="sentences" />
         <Input placeholder="E-mail" autoCapitalize="none" />
         <Input placeholder="Senha" secureTextEntry autoCapitalize="none" />
-        <TouchableOpacity>
-          <Text alignSelf="flex-end" color="gray.400" fontFamily="semiBold" fontSize="xs">Esqueceu a senha?</Text>
-        </TouchableOpacity>
         <Button title="Entrar" mt={12} />
       </VStack>
+
       <ThirdPartyAuth />
+      <Text textAlign="center" mt={6} color="gray.400">
+        ao avançar você confirma os{" "}
+        <Text fontWeight="bold" color="gray.500" underline>
+          termos de uso
+        </Text>{" "}
+        do aplicativo.
+      </Text>
     </VStack>
-  )
+  );
 }
