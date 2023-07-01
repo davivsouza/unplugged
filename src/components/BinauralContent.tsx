efa83796be707dc35748e688ed221acaad24526c
davivsouza <davivasconcelossouza7@gmail.com>
2023-07-01 11:58:07 -0300

    feat: create BinauralSound screen content

---
+++ src/components/BinauralContent.tsx
@@ -0,0 +1,113 @@
+import {
+  HStack,
+  Pressable,
+  Text,
+  VStack,
+  Box,
+  Image,
+  useTheme,
+  ScrollView,
+} from "native-base";
+import { View } from "react-native";
+
+import BeatsBgTemplate from "@assets/binauralsounds/beat-bg-template.png";
+import BtnPlay from "@assets/binauralsounds/btnPlay.svg";
+import { BinauralSoundCard } from "@components/BinauralSoundCard";
+
+
+export function BinauralContent() {
+  const { colors } = useTheme();
+
+  return (
+    <VStack>
+      <HStack justifyContent="space-between" alignItems="center">
+        <Pressable>
+          <Box width={8} height={1} bg="white" mb={2} rounded="md" />
+          <Box width={8} height={1} bg="white" rounded="md" />
+        </Pressable>
+        <Text color="white" fontFamily="semiBold" fontSize="3xl">
+          Binaural Beats
+        </Text>
+        <Image
+          w={10}
+          h={10}
+          rounded="full"
+          source={{
+            uri: "https://e0.pxfuel.com/wallpapers/675/779/desktop-wallpaper-funny-monkeys-pistols-necktie-suit-thumbnail.jpg",
+          }}
+          alt="Foto de perfil do usuário"
+        />
+      </HStack>
+      <Text color="white" fontFamily="semiBold" fontSize="xl" mt={8} mb={4}>
+        Seus beats favoritos
+      </Text>
+      <Box position="relative" h={250}>
+        <VStack alignItems="center">
+          <Image
+            w="full"
+            h={250}
+            rounded="3xl"
+            source={BeatsBgTemplate}
+            alt="Foto de perfil do usuário"
+            position="absolute"
+          />
+          <View
+            style={{
+              width: "94%",
+              height: 250,
+              borderRadius: 20,
+              backgroundColor: colors.gray[400],
+              elevation: -1,
+              position: "absolute",
+              top: 12,
+            }}
+          />
+          <View
+            style={{
+              width: "89%",
+              height: 250,
+              borderRadius: 20,
+              backgroundColor: colors.gray[600],
+              elevation: -2,
+              position: "absolute",
+              top: 22,
+            }}
+          />
+        </VStack>
+        <HStack
+          justifyContent="space-between"
+          alignItems="center"
+          bottom={-0.5}
+          height={75}
+          width="full"
+          borderBottomLeftRadius="3xl"
+          borderBottomRightRadius="3xl"
+          position="absolute"
+          bg="rgba(0, 0, 0, 0.6)"
+          py={1}
+          px={5}
+        >
+          <VStack flex={1}>
+            <Text color="white" fontFamily="semiBold" fontSize="2xl">
+              Leitura Imersiva
+            </Text>
+            <Text color="white" fontFamily="body" fontSize="sm">
+              Jorge Martin
+            </Text>
+          </VStack>
+          <Pressable>
+            <BtnPlay width={45} height={45} />
+          </Pressable>
+        </HStack>
+      </Box>
+
+      <Text color="white" fontFamily="semiBold" fontSize="lg" mt={12}>Lançamentos</Text>
+      <ScrollView h={210} showsVerticalScrollIndicator={false}>
+        <BinauralSoundCard title="Relaxamento" beatsQuantity={10}/>
+        <BinauralSoundCard title="Foco" beatsQuantity={7}/>
+        <BinauralSoundCard title="Criatividade" beatsQuantity={3}/>
+        
+      </ScrollView>
+    </VStack>
+  )
+}
---
+++ src/components/BinauralSoundCard.tsx
@@ -0,0 +1,23 @@
+import { HStack, Image, Text, VStack } from "native-base";
+import BinauralThumb from '@assets/binauralsounds/beat-bg-template.png'
+import DotMenuSvg from '@assets/binauralsounds/dotmenu-icon.svg'
+type Props = {
+  title: string
+  beatsQuantity: number
+}
+
+export function BinauralSoundCard({ beatsQuantity, title }: Props) {
+  return (
+    <HStack flex={1} mt={6}alignItems="center" justifyContent="space-between">
+      <HStack>
+
+      <Image source={BinauralThumb} alt="Thumbnail do som binaural" w={120} h={20} rounded="3xl" mr={4} />
+      <VStack alignItems="flex-start">
+        <Text color="white" fontFamily="body" fontSize="lg">{title}</Text>
+        <Text color="white" fontFamily="body" fontSize="xs">{beatsQuantity} beats</Text>
+      </VStack>
+      </HStack>
+      <DotMenuSvg width={25} height={25}/>
+    </HStack>
+  )
+}
