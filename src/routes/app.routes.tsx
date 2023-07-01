3e22d4c3161574d8df7c671e0c9ae92aa6aec63d
davivsouza <davivasconcelossouza7@gmail.com>
2023-07-01 11:57:51 -0300

    feat: Recriando tabBar

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,3 @@
-import { MyTabBar } from "@components/MyTabBar";
 import {
   createBottomTabNavigator,
   BottomTabNavigationProp,
@@ -8,6 +7,13 @@ import { Module } from "@screens/AppScreens/Journey/Module";
 import { Module as ModuleDTO } from "../@types/module";
 import { ModuleVideo } from "@screens/AppScreens/Journey/ModuleVideo";
 import { BinauralSounds } from "@screens/AppScreens/BinauralSounds";
+
+import JourneySvg from "@assets/Journey-icon.svg";
+import MeditationSvg from "@assets/Meditation-icon.svg";
+import ShopSvg from "@assets/shop-icon.svg";
+import BinauralSvg from "@assets/Binaural-icon.svg";
+import HabitsSvg from "@assets/Habits-icon.svg";
+import { Pressable, useTheme } from "native-base";
 
 type AppRoutes = {
   journey: undefined;
@@ -40,9 +46,11 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
+
+  const { colors } = useTheme()
   return (
     <Navigator
-      tabBar={(props) => <MyTabBar {...props} />}
+      // tabBar={(props) => <MyTabBar {...props} />}
       initialRouteName="journey"
       screenOptions={{
         headerShown: false,
@@ -53,16 +61,72 @@ export function AppRoutes() {
           backgroundColor: "transparent",
           shadowColor: "transparent",
           borderTopColor: "transparent",
-          zIndex: 1
+          zIndex: 1,
+          elevation: 0,
+          bottom: 20,
         },
       }}
     >
-      <Screen name="habits" component={Journey} />
-      <Screen name="binaural" component={BinauralSounds}/>
+      <Screen name="habits" component={Journey} options={{
+        tabBarIcon: ({ focused }) => (
+          <HabitsSvg
+            stroke={focused ? colors.purple[500] : "#fff"}
+            strokeWidth={3}
+            width={30}
+            height={30}
+          />
+        ),
+      }} />
+      <Screen name="binaural" component={BinauralSounds} options={{
+        tabBarIcon: ({ focused }) => (
+          <BinauralSvg
+            fill={
+             focused ? colors.purple[500] : "#fff"
+            }
+            width={40}
+            height={40}
+          />
+        )
+      }} />
 
-      <Screen name="journey" component={Journey} />
-      <Screen name="meditation" component={Journey} />
-      <Screen name="shop" component={Journey} />
+      <Screen name="journey" component={Journey} options={{
+        tabBarIcon: ({focused}) => (
+          <Pressable
+              width={16}
+              height={16}
+              bg="purple.500"
+              rounded="full"
+              alignItems="center"
+              justifyContent="center"
+              
+            >
+              <JourneySvg fill="#fff" width={28} height={28} />
+            </Pressable>
+        )
+
+      }}/>
+      <Screen name="meditation" component={Journey} options={{
+        tabBarIcon: ({ focused }) => (
+          <MeditationSvg
+            fill={
+             focused ? colors.purple[500] : "#fff"
+            }
+            width={40}
+            height={40}
+          />
+        )
+      }}/>
+      <Screen name="shop" component={Journey} options={{
+        tabBarIcon: ({ focused }) => (
+          <ShopSvg
+            fill={
+             focused ? colors.purple[500] : "#fff"
+            }
+            width={30}
+            height={30}
+          />
+        )
+      }}/>
       <Screen
         name="module"
         component={Module}
--- src/screens/AppScreens/BinauralSounds/index.tsx
+++ src/screens/AppScreens/BinauralSounds/index.tsx
@@ -1,18 +1,11 @@
 import { useState } from "react";
 import {
-  HStack,
-  Pressable,
   Text,
   VStack,
-  Box,
-  Image,
-  useTheme,
 } from "native-base";
 import { BinauralForm } from "./BinauralForm";
-import { View } from "react-native";
 
-import BeatsBgTemplate from "@assets/binauralsounds/beat-bg-template.png";
-import BtnPlay from "@assets/binauralsounds/btnPlay.svg";
+import { BinauralContent } from "@components/BinauralContent";
 
 export function BinauralSounds() {
   const [categories, setCategories] = useState<String[]>([
@@ -20,7 +13,6 @@ export function BinauralSounds() {
     "dasgdsahd",
     "dhasjhueh",
   ]);
-  const { colors } = useTheme();
 
   return (
     <VStack
@@ -36,88 +28,7 @@ export function BinauralSounds() {
       }}
     >
       {categories.length > 0 ? (
-        <VStack>
-          <HStack justifyContent="space-between" alignItems="center">
-            <Pressable>
-              <Box width={8} height={1} bg="white" mb={2} rounded="md" />
-              <Box width={8} height={1} bg="white" rounded="md" />
-            </Pressable>
-            <Text color="white" fontFamily="semiBold" fontSize="3xl">
-              Binaural Beats
-            </Text>
-            <Image
-              w={10}
-              h={10}
-              rounded="full"
-              source={{
-                uri: "https://e0.pxfuel.com/wallpapers/675/779/desktop-wallpaper-funny-monkeys-pistols-necktie-suit-thumbnail.jpg",
-              }}
-              alt="Foto de perfil do usuário"
-            />
-          </HStack>
-          <Text color="white" fontFamily="semiBold" fontSize="xl" mt={8} mb={4}>
-            Seus beats favoritos
-          </Text>
-          <Box position="relative" h={250}>
-            <VStack alignItems="center">
-              <Image
-                w="full"
-                h={250}
-                rounded="3xl"
-                source={BeatsBgTemplate}
-                alt="Foto de perfil do usuário"
-                position="absolute"
-              />
-              <View
-                style={{
-                  width: "94%",
-                  height: 250,
-                  borderRadius: 20,
-                  backgroundColor: colors.gray[400],
-                  elevation: -1,
-                  position: "absolute",
-                  top: 12,
-                }}
-              />
-              <View
-                style={{
-                  width: "89%",
-                  height: 250,
-                  borderRadius: 20,
-                  backgroundColor: colors.gray[600],
-                  elevation: -2,
-                  position: "absolute",
-                  top: 22,
-                }}
-              />
-            </VStack>
-            <HStack
-              justifyContent="space-between"
-              alignItems="center"
-              bottom={-0.5}
-              height={75}
-              width="full"
-              borderBottomLeftRadius="3xl"
-              borderBottomRightRadius="3xl"
-              position="absolute"
-              bg="rgba(0, 0, 0, 0.6)"
-              py={1}
-              px={5}
-            >
-              <VStack flex={1}>
-                <Text color="white" fontFamily="semiBold" fontSize="2xl">
-                  Leitura Imersiva
-                </Text>
-                <Text color="white" fontFamily="body" fontSize="sm">
-                  Jorge Martin
-                </Text>
-              </VStack>
-              <Pressable>
-                <BtnPlay width={45} height={45} />
-              </Pressable>
-            </HStack>
-          </Box>
-        </VStack>
+        <BinauralContent />
       ) : (
         <BinauralForm setCategories={setCategories} />
       )}
