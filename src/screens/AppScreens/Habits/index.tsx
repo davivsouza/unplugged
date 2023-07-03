43093b29a944dec2fbc607bd831a5df857ed8fd8
davivsouza <davivasconcelossouza7@gmail.com>
2023-07-03 00:58:40 -0300

    feat: create header of Habits screen

--- src/screens/AppScreens/Habits/index.tsx
+++ src/screens/AppScreens/Habits/index.tsx
@@ -1,6 +1,18 @@
-import { Text, VStack } from "native-base";
+import { HabitsContent } from "@components/HabitsContent";
+import { HabitsFloatButton } from "@components/HabitsFloatButton";
+import { HabitsNavigation } from "@components/HabitsNavigation";
+import { HStack, Image, Text, VStack } from "native-base";
+import {useState} from 'react'
 
+export type HabitsItems = "insights" | "metas" | "controle"
 export function Habits() {
+
+  const [selectedItem, setSelectedItem] = useState<HabitsItems>("insights");
+
+  function handleSelectedItem(item: HabitsItems){
+    setSelectedItem(item)
+  }
+
   return (
     <VStack
       flex={1}
@@ -13,10 +25,27 @@ export function Habits() {
           end: [0, 1],
         },
       }}
+      position="relative"
     >
-      <Text color="white" fontFamily="heading" fontSize="3xl" textAlign="center">
-        HÁBITOS
-      </Text>
+      <HStack alignItems="center" justifyContent="center" position="relative" mb={12}>
+        <Text color="white" fontFamily="semiBold" fontSize="3xl" textAlign="center">
+          Hábitos
+        </Text>
+        <Image
+          w={12}
+          h={12}
+          rounded="full"
+          source={{
+            uri: "https://e0.pxfuel.com/wallpapers/675/779/desktop-wallpaper-funny-monkeys-pistols-necktie-suit-thumbnail.jpg",
+          }}
+          alt="Foto de perfil do criador do vídeo"
+          position="absolute"
+          right={0}
+        />
+      </HStack>
+      <HabitsNavigation selectedItem={selectedItem} onSelectItem={handleSelectedItem}/>
+      <HabitsFloatButton />
+      <HabitsContent item={selectedItem} />
     </VStack>
   )
 }
