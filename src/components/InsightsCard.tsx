ba212f3b353258acf7e33ae0b25a4bd900c0c44a
davivsouza <davivasconcelossouza7@gmail.com>
2023-07-07 21:18:24 -0300

    style: change css

--- src/components/InsightsCard.tsx
+++ src/components/InsightsCard.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export function InsightsCard({children}:Props){
   return (
-    <VStack w="full" h={320} alignItems="center" justifyContent="center" bg="gray.500" rounded="xl" mb={12} py={12}  px={4}>
+    <VStack w="full" h={320} alignItems="center" justifyContent="center" bg="gray.500" rounded="xl" mb={12} px={4}>
       {children}
     </VStack>
   )
