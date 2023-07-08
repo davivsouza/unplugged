143f164b3b9549fe8da321f85f7d6a3e4cbaa663
davivsouza <davivasconcelossouza7@gmail.com>
2023-07-07 21:18:01 -0300

    chore: add FloatButton to a specific component

--- src/components/HabitsMetas.tsx
+++ src/components/HabitsMetas.tsx
@@ -1,9 +1,13 @@
-import { Text } from "native-base";
+import { Text, VStack } from "native-base";
+import { HabitsFloatButton } from "./HabitsFloatButton";
 
 export function HabitsMetas(){
   return (
+    <VStack position="relative">
     <Text color="white" fontFamily="body" fontSize="xl">
       Metas
     </Text>
+      <HabitsFloatButton/>
+    </VStack>
   )
 }
